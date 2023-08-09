ed919b2f609aed5561337f8dea528970c59d259c
yoncho <joyonghyeon13@gmail.com>
2023-08-09 17:55:25 +0900

    practice03-02 update

--- practice03/src/main/java/prob02/GoodsApp.java
+++ practice03/src/main/java/prob02/GoodsApp.java
@@ -3,41 +3,43 @@ package prob02;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class GoodsApp {
-	private static final int COUNT_GOODS = 3;
-	public static class Goods{
-		private String goodsName;
-		private int goodsPrice;
-		private int goodsInputCount;
-		public String getGoodsName() {
-			return goodsName;
-		}
-		public void setGoodsName(String goodsName) {
-			this.goodsName = goodsName;
-		}
-		public int getGoodsPrice() {
-			return goodsPrice;
-		}
-		public void setGoodsPrice(int goodsPrice) {
-			this.goodsPrice = goodsPrice;
-		}
-		public int getGoodsInputCount() {
-			return goodsInputCount;
-		}
-		public void setGoodsInputCount(int goodsInputCount) {
-			this.goodsInputCount = goodsInputCount;
-		}
-		public void printGoodsInfo()
-		{
-			System.out.println(getGoodsName() +"(가격:"
-					+ getGoodsPrice() +"원)이 "
-					+ getGoodsInputCount()+"개 입고 되었습니다.");
-		}
+class Goods{
+	private String goodsName;
+	private int goodsPrice;
+	private int goodsInputCount;
+	
+	public String getGoodsName() {
+		return goodsName;
 	}
+	public void setGoodsName(String goodsName) {
+		this.goodsName = goodsName;
+	}
+	public int getGoodsPrice() {
+		return goodsPrice;
+	}
+	public void setGoodsPrice(int goodsPrice) {
+		this.goodsPrice = goodsPrice;
+	}
+	public int getGoodsInputCount() {
+		return goodsInputCount;
+	}
+	public void setGoodsInputCount(int goodsInputCount) {
+		this.goodsInputCount = goodsInputCount;
+	}
+	public void printGoodsInfo()
+	{
+		System.out.println(getGoodsName() +"(가격:"
+				+ getGoodsPrice() +"원)이 "
+				+ getGoodsInputCount()+"개 입고 되었습니다.");
+	}
+}
+
+public class GoodsApp {
+	private static int COUNT_GOODS = 3;
+	
 	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-
 		Goods[] goods = new Goods[COUNT_GOODS];
 		// 상품 입력
 		System.out.println("실행 결과:");
