d0dcb0b7a4bd5405ea320897aa8638f49240ba16
yoncho <joyonghyeon13@gmail.com>
2023-08-10 17:16:56 +0900

    study - tv

--- chapter03/src/main/java/tv/TV.java
+++ chapter03/src/main/java/tv/TV.java
@@ -1,12 +1,29 @@
 package tv;
 
+
 public class TV {
+	//MIN, MAX VALUE
+	static int MIN_CHANNEL = 1;
+	static int MAX_CHANNEL = 255;
+	static int MIN_VOLUME = 0;
+	static int MAX_VOLUME = 100;
+	
 	private int channel; //단, 1~255 (overflow시 1로,, underflow시 255로)
 	private int volume; //단, 0 ~ 100 (overflow시 0, underflow시 100)
  	private boolean power;
  	
- 	public TV() {
+ 	public TV(int channel, int volume, boolean power) {
+ 		this.channel = channel;
+ 		this.volume = volume;
+ 		this.power = power;
  		
+ 	}
+ 	public int getChannel(){
+ 		return channel;
+ 	}
+ 	
+ 	public int getVolume() {
+ 		return volume;
  	}
  	
  	public void power(boolean on){
@@ -15,25 +32,67 @@ public class TV {
  	
  	public void volume(boolean up) {
  		//current power check logic
+ 		if (power) {
+ 			this.volume = checkRange(MIN_VOLUME, MAX_VOLUME, this.volume, 1);
+ 		}
+ 		else {
+ 			exceptionPowerOff();
+ 		}
+ 		
  	}
  	
  	public void volume(int volume) {
- 		this.volume = volume;
+ 		if (power) {
+ 			this.volume = checkRange(MIN_VOLUME, MAX_VOLUME, this.volume, volume);
+ 		}
+ 		else {
+ 			exceptionPowerOff();
+ 		}
  	}
  	
  	public void channel(int channel) {
- 		this.channel = channel;
+ 		if (power) {
+ 			this.channel = checkRange(MIN_VOLUME, MAX_VOLUME, this.channel, channel);
+ 		}
+ 		else {
+ 			exceptionPowerOff();
+ 		}
  	}
  	
  	public void channel(boolean up) {
- 		
+ 		if (power) {
+ 			this.channel = checkRange(MIN_VOLUME, MAX_VOLUME, this.channel, 1);
+ 		}
+ 		else {
+ 			exceptionPowerOff();
+ 		}
  	}
  	
  	public void status() {
  		System.out.println("TV[power = " + (power ? "on":"off") + 
- 				", channel= " + channel + ", volume= " + volume + "]");
+ 				", channel= " + getChannel() + ", volume= " + getVolume() + "]");
  	}
  	
+ 	public int checkRange(int min, int max, int currentValue, int upCount)
+ 	{
+ 		int checkValue = currentValue + upCount;
+ 		int result;
+ 		
+ 		if(checkValue > max) {
+ 			result = min;
+ 		}else if (checkValue < min)
+ 		{
+ 			result = max;
+ 		}
+ 		else
+ 		{
+ 			result = checkValue;
+ 		}
+ 		
+ 		return result;
+ 	}
  	
- 	
+ 	public void exceptionPowerOff() {
+ 		System.out.println("TV가 꺼저 있습니다.");
+ 	}
 }
