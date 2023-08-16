cb6174aeab952506aea13dbcf8899ac8c6486a0f
yoncho <joyonghyeon13@gmail.com>
2023-08-16 17:30:57 +0900

    study - final test update

--- finaltest/src/main/java/prob05/PasswordDismatchException.java
+++ finaltest/src/main/java/prob05/PasswordDismatchException.java
@@ -3,7 +3,6 @@ package prob05;
 @SuppressWarnings("serial")
 public class PasswordDismatchException extends RuntimeException{
 	public PasswordDismatchException() {
-		super("Password Dismatch");
 	}
 	
 	public PasswordDismatchException(String message) {
--- finaltest/src/main/java/prob05/UserNotFoundException.java
+++ finaltest/src/main/java/prob05/UserNotFoundException.java
@@ -3,7 +3,6 @@ package prob05;
 @SuppressWarnings("serial")
 public class UserNotFoundException extends RuntimeException{
 	public UserNotFoundException() {
-		super("User Not Found");
 	}
 	public UserNotFoundException(String message) {
 		super(message);
