fe4c243ea7edf28635e7a67ec714b1bbb2cf1899
yoncho <joyonghyeon13@gmail.com>
2023-08-22 12:28:09 +0900

    study - chatting gui

--- network/src/main/java/chat/gui/ChatWindow.java
+++ network/src/main/java/chat/gui/ChatWindow.java
@@ -35,16 +35,35 @@ public class ChatWindow {
 		// Button
 		buttonSend.setBackground(Color.GRAY);
 		buttonSend.setForeground(Color.WHITE);
-		buttonSend.addActionListener( new ActionListener() {
+		buttonSend.addActionListener(new ActionListener() {
+
 			@Override
-			public void actionPerformed( ActionEvent actionEvent ) {
+			public void actionPerformed(ActionEvent e) {
+				//sendMessage();
 				sendMessage();
 			}
+			
+		});
+		//위와 아래가 같음.. 추정하는것!추론하는것!
+		buttonSend.addActionListener((ActionEvent e)->{
+			
 		});
 
 		// Textfield
 		textField.setColumns(80);
+		textField.addKeyListener(new KeyAdapter() {
 
+			@Override
+			public void keyPressed(KeyEvent e) {
+				char keyCode = e.getKeyChar();
+				
+				if(keyCode == KeyEvent.VK_ENTER) {
+					sendMessage();
+				}
+			}
+			
+		});
+		
 		// Pannel
 		pannel.setBackground(Color.LIGHT_GRAY);
 		pannel.add(textField);
@@ -58,20 +77,46 @@ public class ChatWindow {
 		// Frame
 		frame.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
-				System.exit(0);
+				finish();
+				
 			}
 		});
 		frame.setVisible(true);
 		frame.pack();
+		
+		// IOStream 받아오기
+		// ChatClientThread 생성 및 실행
+	}
+	
+	private void finish() {
+		//quit 프로토콜 구현
+		
+		//exit java(jvm)
+		System.exit(0); // jvm 종료.. 0(정상종료)/1(비정상종료)
 	}
 	
 	private void sendMessage() {
+		String message = textField.getText();
+		
+		System.out.println("Messaeg Send Protocol :" + message);
+		
+		textField.setText("");
+		textField.requestFocus(); //마우스 포커스 가저오기
+		
+		//ChatClientThread 에서 서버로 부터 받은 메세지가 있다고 가정하고..
+		updateTextArea("마이콜 : " + message);
+	}
+	
+	private void updateTextArea(String message) {
+		textArea.append(message);
+		textArea.append("\n");
 	}
 	
 	private class ChatClientThread extends Thread{
 
 		@Override
 		public void run() {
+			updateTextArea("마이콜: 안녕");//받은 데이터
 			
 		}
 		
